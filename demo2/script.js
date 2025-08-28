943d8c98c52be4bcecbc2a431e21716a845a86da
SamElias001 <samueliasps2001@hotmail.com>
2025-08-27 21:24:54 -0300

    12/08/2025
    Introduces demo2/index.html and demo2/script.js with basic examples of JavaScript data types, including string, number, boolean, array, and object usage. The HTML file loads the script for demonstration purposes.

---
+++ demo2/script.js
@@ -0,0 +1,25 @@
+//  String
+let nome = "Samuel";
+console.log(nome);
+console.log(`O meu nome é ${nome}`);
+
+// Number (número inteiro ou decimal):
+let idade = 23;
+console.log(idade);
+let salario = 1000.01;
+console.log(salario);
+console.log(`O meu nome é ${nome}, tenho ${idade} anos e atualmente ganho ${salario} reais.`);
+
+// Boolean (true ou false):
+let estaLogado = true;
+console.log(estaLogado);
+
+// Array (lista de valores):
+let frutas = ["Maça","Banana","Uva"];
+console.log(`O primeiro valor de array frutas é ${frutas[0]}`);
+console.log(`O segundo valor de array frutas é ${frutas[1]}`);
+console.log(`O terceiro valor de array frutas é ${frutas[2]}`);
+
+// Obejeto (estrutura de dados chave-valor):
+let pessoa = { nome: "Carlos", idade: 30, profissao: "Engenehiro"};
+console.log(`O meu nome é ${pessoa.nome}, tenho ${pessoa.idade} anos e sou ${pessoa.profissao}`);
